b71656e274f654570094718541cb558e45161c77
root <root@newerit.com>
2015-07-26 20:23:01 -0400

    go to trunk

---
+++ trunk/rd_legacy/app/patch/ExtJS411aPatch.js
@@ -0,0 +1,8 @@
+Ext.define('Rd.patch.ExtJS411aPatch', {
+    requires: [
+        'Rd.patch.form.SubmitFix',
+        'Rd.patch.view.GridLoadMask',
+        'Rd.patch.window.Window',
+        'Rd.patch.data.Store'
+    ]
+});
---
+++ trunk/rd_legacy/app/view/Viewport.js
@@ -0,0 +1,5 @@
+Ext.define('Rd.view.Viewport', {
+    extend: 'Ext.container.Viewport',
+    alias: 'widget.vp',
+    layout: 'fit'
+});
